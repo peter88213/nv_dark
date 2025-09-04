ea12a5b529f582e406b30ec8e864bcaf8a6a74df
peter88213 <petert88213@gmail.com>
2025-09-04 12:57:23 +0200

    Updated docstring: Python 3.7+ required.

--- src/nv_dark.py
+++ src/nv_dark.py
@@ -2,7 +2,7 @@
 
 Applies the 'awdark' theme, if available. 
 
-Requires Python 3.6+
+Requires Python 3.7+
 Copyright (c) 2025 Peter Triesberger
 For further information see https://github.com/peter88213/nv_dark
 License: GNU GPLv3 (https://www.gnu.org/licenses/gpl-3.0.en.html)
